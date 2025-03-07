0a8eed655033f4699c0aca15eef9c4ac1751571c
rohitsinghpanwar <rohitsinghpanwar108@gmail.com>
2025-03-07 16:02:02 +0530

    seo changes

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
 
       <div className="flex flex-col md:flex-row w-full justify-center items-center px-4">
         <form onSubmit={handlesubmit} className="border-2 border-red-600 rounded-xl flex flex-col p-5 m-2 w-full md:w-[40vw] text-xl gap-4 h-[50vh]">
-          <p className='font-bold'>Save & Reuse – Instantly Access Your Frequently Used Text & Links!</p>
+          <p className='font-bold italic'>Save & Reuse – Instantly Access Your Frequently Used Text & Links!</p>
           <label className="flex flex-col w-full">Name
             <input type="text" onChange={handlechange} name="name" value={details.name} className="bg-violet-800 rounded-lg h-12 w-full px-2" required placeholder="Enter the name of the text" />
           </label>
@@ -74,11 +74,11 @@ function App() {
           <button type="submit" className="border-white border text-white py-2 px-4 rounded-lg hover:bg-white/50 transition">Save</button>
         </form>
 
-        <ul className="w-full md:w-[40vw] h-[50vh] overflow-y-scroll border-2 border-red-600 rounded-xl p-2 m-2 flex flex-col items-center gap-2 md:text-lg">
-          <h2 className="text-2xl font-bold">Saved Links</h2>
+        <ul className="w-full md:w-[40vw] h-[50vh] overflow-y-scroll border-2 border-red-600 rounded-xl p-2 m-2 flex flex-col items-center gap-2 scroll-smooth   md:text-lg">
+          <h2 className="text-2xl font-bold italic">Saved Links</h2>
           {Object.entries(savedlinks).length > 0 ? (
             Object.entries(savedlinks).map(([key, value]) => (
-              <li key={key} className="border w-full flex flex-col md:flex-row items-center justify-between p-1 bg-violet-800 transition-all ease-in-out duration-500 rounded-xl hover:shadow-sm shadow-white md:p-2 ">
+              <li key={key} className="border w-full flex flex-col md:flex-row items-center justify-between p-1 bg-violet-800 transition-all ease-in-out duration-500 rounded-xl hover:shadow-sm shadow-white md:p-2">
                 <strong className="text-center md:text-left">{key}</strong>
                 <h1 className="break-all text-center md:text-left">{value}</h1>
                 <div className="flex gap-2 mt-2 md:mt-0">
